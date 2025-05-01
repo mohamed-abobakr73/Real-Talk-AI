549414607201bbc836998cbae3b0bd079bd25462
= <=>
2025-05-01 19:27:58 +0200

    added the send otp to email functionality to the signup route

--- user-service/src/controllers/authController.ts
+++ user-service/src/controllers/authController.ts
@@ -3,6 +3,7 @@ import asyncHandler from "../middlewares/asyncHandler";
 import authServices from "../services/authServices";
 import httpStatusText from "../utils/httpStatusText";
 import generateJWT from "../utils/jwtUtils/generateJWT";
+import sendOtpToEmail from "../utils/otpUtils/sendOtpToEmail";
 
 const signup = asyncHandler(
   async (
@@ -11,21 +12,36 @@ const signup = asyncHandler(
     next: NextFunction
   ): Promise<Response | void> => {
     const validatedRequestBody = req.validatedData;
+    const { email, username } = validatedRequestBody;
     if (req.file) validatedRequestBody.profileImage = req.file.buffer;
 
     const createdUser = await authServices.signupService(validatedRequestBody);
-    const tokenPayload = {
-      id: createdUser.userId,
-      email: createdUser.email,
-    };
 
-    const token = generateJWT(tokenPayload);
+    // const tokenPayload = {
+    //   id: createdUser.userId,
+    //   email: createdUser.email,
+    // };
+
+    // const token = generateJWT(tokenPayload);
+
+    const sendEmail = await sendOtpToEmail(email, username);
 
     return res.status(201).json({
       status: httpStatusText.SUCCESS,
-      data: { user: createdUser, token },
+      data: {
+        message: `Account created successfully, Please verify your email using the OTP sent to ${email}`,
+      },
     });
   }
 );
 
-export { signup };
+const verifyOtp = asyncHandler(async (req: Request, res: Response) => {
+  return res.status(200).json({
+    status: httpStatusText.SUCCESS,
+    data: {
+      message: "OTP verified successfully",
+    },
+  });
+});
+
+export { signup, verifyOtp };
