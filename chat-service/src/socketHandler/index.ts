6c24c7f64639f16b906f6f02345ecec424f5d04b
= <=>
2025-05-13 22:47:05 +0200

    added join chat handler

--- chat-service/src/socketHandler/index.ts
+++ chat-service/src/socketHandler/index.ts
@@ -1,8 +1,8 @@
-import { ExtendedError, Server } from "socket.io";
+import { Server } from "socket.io";
 import http from "http";
 import verifySocketAccessToken from "../middlewares/verifySocketAccessToken";
-import receiveAndSaveChatMessage from "../utils/receiveAndSaveChatMessage";
 import registerMessageHandler from "./registerMessageHandler";
+import joinChatHandler from "./joinChatHandler";
 
 const setupSocket = (server: http.Server) => {
   const io = new Server(server, {
@@ -18,15 +18,7 @@ const setupSocket = (server: http.Server) => {
     console.log("✅ New client connected:", socket.id);
 
     // // Join chat room
-    socket.on("join_chat", async (chatId) => {
-      // const chat = await chatsServices.getChatService(chatId);
-      // chatsServices.checkIfUserIsPartOfChat(
-      //   chat.users,
-      //   socket.data.user.userId
-      // );
-      socket.join(chatId);
-      console.log("Joined chat room:", chatId);
-    });
+    joinChatHandler(socket);
 
     registerMessageHandler(socket, io);
 
---
+++ chat-service/src/socketHandler/joinChatHandler.ts
@@ -0,0 +1,33 @@
+import { Socket } from "socket.io";
+import chatsServices from "../services/chatsServices";
+import globalError from "../utils/globalError";
+import httpStatusText from "../utils/httpStatusText";
+
+const joinChatHandler = (socket: Socket) => {
+  socket.on("join_chat", async (chatId, callback) => {
+    try {
+      if (!chatId) {
+        const error = globalError.create(
+          "Chat id not found",
+          404,
+          httpStatusText.NOT_FOUND
+        );
+        callback(error);
+      }
+
+      const chat = await chatsServices.getChatService(chatId);
+
+      chatsServices.checkIfUserIsPartOfChat(
+        chat.users,
+        socket.data.user.userId
+      );
+
+      socket.join(chatId);
+      console.log("Joined chat room:", chatId);
+    } catch (error) {
+      callback(error);
+    }
+  });
+};
+
+export default joinChatHandler;
