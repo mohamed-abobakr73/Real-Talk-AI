e3942722ad082b9f876c47deb5e5df63116da872
= <=>
2025-05-22 19:38:15 +0200

    fixed port

--- ai-service/src/index.ts
+++ ai-service/src/index.ts
@@ -18,7 +18,7 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(helmet());
 
-app.use("/ai", aiRouter);
+app.use("/api/v1/ai", aiRouter);
 
 app.use(
   (error: TGlobalError, req: Request, res: Response, next: NextFunction) => {
@@ -31,6 +31,6 @@ app.use(
   }
 );
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 4001, () => {
   console.log(`Server is running on port ${PORT}`);
 });
