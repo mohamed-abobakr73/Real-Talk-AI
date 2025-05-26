aabd0197e7045640657de27e02ad1b6975e91aee
= <=>
2025-05-26 18:46:03 +0200

    checking if the subscription exists before creating a new one

--- notification-service/src/services/notificationsServices.ts
+++ notification-service/src/services/notificationsServices.ts
@@ -2,10 +2,29 @@ import webpush from "../config/webPushConfig";
 import PushSubscriptionsModel from "../models/PushSubscriptionsModel";
 import { TNotificationSubscribe } from "../schemas";
 
+const getSubscriptionByUserIdService = async (userId: string) => {
+  try {
+    const subscription = await PushSubscriptionsModel.findOne({ userId });
+    return subscription;
+  } catch (error) {
+    throw error;
+  }
+};
+
 const savePushSubscriptionDataService = async (
-  pushData: TNotificationSubscribe
+  pushData: TNotificationSubscribe & { userId: string }
 ) => {
   try {
+    const { userId } = pushData;
+    const subscription = await PushSubscriptionsModel.findOne({
+      userId,
+      endpoint: pushData.endpoint,
+    });
+
+    if (subscription) {
+      return subscription;
+    }
+
     const pushSubscription = await PushSubscriptionsModel.create(pushData);
     return pushSubscription;
   } catch (error) {
