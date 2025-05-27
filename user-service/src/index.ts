dd45ff501725cfa3f552f8dfeab12379e020c507
= <=>
2025-05-27 18:52:01 +0200

    setting the request limit to 1mb on json files and form data in user service

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -14,7 +14,8 @@ configDotenv();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(cors());
