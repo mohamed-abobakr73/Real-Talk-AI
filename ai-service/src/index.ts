b86bc36712ea18877aeaa1b30afc1bdb7b5cbbd0
= <=>
2025-05-27 21:13:39 +0200

    added request size limit to ai service

--- ai-service/src/index.ts
+++ ai-service/src/index.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(morgan("dev"));
 app.use(cors());
 app.use(helmet());
